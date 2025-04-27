4042404539d9eb59b30be3407ee2574477b8b4f5
Jasmine Huang <jasmine12122005@yahoo.com.tw>
2025-04-26 20:15:42 -0500

    modified job submit structure

--- src/worker.py
+++ src/worker.py
@@ -1,21 +1,62 @@
 import time
+import json
 from jobs import get_job_by_id, update_job_status
-from redis_client import q
+from utils import generate_magnitude_histogram_bytes
+from redis_client import q, res
 from logger_config import get_logger
 
 logger = get_logger(__name__)
+
+JOB_HANDLERS = {
+    'magnitude_distribution': generate_magnitude_histogram_bytes
+}
 
 @q.worker
 def do_work(jid: str) -> None:
     logger.info(f"Processing job: {jid}")
     update_job_status(jid, 'in progress')
 
-    job_data = get_job_by_id(jid)
-    job_type = job_data.get('type')
-    logger.info(f"Job {jid} type: {job_type}")
+    try:
+        job_data = get_job_by_id(jid)
+        if not job_data:
+            raise ValueError(f"No job data found for jid: {jid}")
 
-    time.sleep(10)   # there is no real work for now
-    update_job_status(jid, 'complete')
+        job_type = job_data.get('type')
+        start_date = job_data.get('start')
+        end_date = job_data.get('end')
+
+        logger.info(f"Job {jid} type: {job_type}")
+
+        if not start_date or not end_date:
+            raise ValueError("Missing start or end date") 
+
+        handler = JOB_HANDLERS.get(job_type)
+        if not handler:
+            raise ValueError(f"Unsupported job type: {job_type}")
+
+        results = handler(start_date, end_date)
+        logger.info(f"Results type for job {jid}: {type(results)}")
+
+        # storage depends on result type
+        if isinstance(results, dict):
+            res.hset(jid, mapping={
+                'type': 'json',
+                'content': json.dumps(results),
+            })
+        elif isinstance(results, bytes):
+            res.hset(jid, mapping={
+                'type': 'image',
+                'content': results,
+            })
+        else:
+            raise ValueError(f"Unsupported result type for job {jid}.")
+
+        update_job_status(jid, 'complete')
+        logger.info(f"Job {jid} completed.")
+
+    except Exception as e:
+        logger.exception(f"Job {jid} failed: {e}")
+        update_job_status(jid, 'failed')
 
 if __name__ == "__main__":
     logger.info("Worker is listening for jobs...")
